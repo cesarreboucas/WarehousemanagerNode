f0b4f7aa91b5c8ead1ef382bb39e595183ca97b5
Oliveira Reboucas <oliveirareboucasc@student.douglascollege.ca>
2019-10-30 20:01:20 -0700

    Seed Users

--- routes/routes.js
+++ routes/routes.js
@@ -7,24 +7,6 @@ router.get('/', function(req, res) {
     res.send({"working":1});
 });
 
-router.get('/seed', function(req, res) {
-    const fs = require("fs");
-    async function readfile() {
-        const content = fs.readFileSync("./data/seedUsers.js");
-        const users = JSON.parse(content);
-        users.forEach(user => {
-            usersController.mySqlcreateUser(user, function(err, r) {
-                console.log("Creating User "+user.name+" id: "+r.id);
-            });
-            
-        });
-    }
-    readfile();
-    console.log("Done");
-    res.send({"working":1});
-});
-
-
 /* GET users listing. */
 router.get('/products', productsController.products_list);
 router.post('/products', productsController.products_add);
@@ -36,6 +18,61 @@ router.get('/users', usersController.users_list);
 router.get('/users/auth', usersController.users_auth);
 /* Create a new user. */
 router.post('/users', usersController.users_add);
+
+
+/* ####### SEED ############*/
+
+router.get('/seed', function(req, res) {
+    const fs = require("fs");
+    let mysql = require("../services/mysql");
+    const async = require('async');
+
+    async.waterfall([
+        function(callback) {
+            mysql.query('CREATE TABLE IF NOT EXISTS `users` ( \
+                `id` int NOT NULL AUTO_INCREMENT, \
+                `name` varchar(255) NOT NULL, \
+                `username` varchar(255) NOT NULL, \
+                `password` varchar(255) NOT NULL, \
+                `role` varchar(255) NOT NULL, \
+                `question` varchar(255) NOT NULL, \
+                `answer` varchar(255) NOT NULL, \
+                PRIMARY KEY (`id`) \
+              );', function(error, results, fields) {
+                if(error) console.log(error);
+                else {
+                    console.log("Create table user");
+                    callback(null);
+                }
+            });        
+        },
+        function (callback) {
+            console.log("Reading the file");
+            let content = fs.readFileSync("./data/seedUsers.js","utf8");
+            //console.log(content);
+            callback(null, content);
+        },
+        function(content, callback) {
+            console.log("Parse the file");
+            
+            const users = JSON.parse(content);    
+            console.log(users);
+            users.forEach(user => {
+                console.log(user);
+                usersController.mySqlcreateUser(user, function(err, r) {
+                    console.log("Creating User "+user.name+" id: "+r);
+                });
+            //callback(null);
+                
+            });
+        }
+
+    ], function (err, result) {
+    });
+    
+    console.log("Done");
+    res.send({"working":1});
+});
 
 module.exports = router;
 
--- services/mysql.js
+++ services/mysql.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 const mysql = require('mysql');
 
 let connection = mysql.createConnection({
-=======
-const mysql      = require('mysql');
-const connection = mysql.createConnection({
->>>>>>> a4f3b800e587983da5525e066f9769b820a17d8d
   host     : 'localhost',
   user     : 'root',
   password : '',
@@ -13,7 +8,6 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
-<<<<<<< HEAD
 /*
 connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
   if (error) throw error;
@@ -23,6 +17,4 @@ connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
 
 //connection.end();
 
-=======
->>>>>>> a4f3b800e587983da5525e066f9769b820a17d8d
 module.exports = connection;
